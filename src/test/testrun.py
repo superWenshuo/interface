d49b609dfb246b4b36495d71dd2fe2a6ece9434d
superWenshuo <986012014@qq.com>
2019-08-26 21:00:05 +0800

    1

--- src/test/testrun.py
+++ src/test/testrun.py
@@ -2,27 +2,23 @@
 #jenkins运行项目需要添加sys.path.append
 import sys
 # print (sys.path)
-# sys.path.append('/root/.jenkins/workspace/test/src/test')
+sys.path.append('/root/.jenkins/workspace/test/src/common')
 # sys.path.append('/usr/local/python3.5/lib/python35.zip')
 # sys.path.append('/usr/local/python3.5/lib/python3.5')
 # sys.path.append('/usr/local/python3.5/lib/python3.5/plat-linux')
 # sys.path.append('/usr/local/python3.5/lib/python3.5/lib-dynload')
 # sys.path.append('/usr/local/python3.5/lib/python3.5/site-packages')
 
-
-
-
-
-
 import unittest
-from common.excel import read_excel
+# from common.excel import read_excel
+import common.excel
 from common import base
 
 class TestRun(unittest.TestCase):
     
 
     def run(self):
-        read_excel(base.RunMethod().get_case_url())
+        common.excel.read_excel(base.RunMethod().get_case_url())
         #执行测试用例
         
  
